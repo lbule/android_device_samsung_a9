353c314e78b2cbb9df37046e9b12132b5fc12845
Louis Popi <theh2o64@gmail.com>
2015-12-27 23:20:52 +0100

    a9: Updates from  LA.BR.1.3.3-01420-8976.0 (06.00.01)

--- Android.mk
+++ Android.mk
@@ -262,4 +262,12 @@ $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
         $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat; \
         ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
         $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
+	mkdir -p $(TARGET_OUT_ETC)/wifi \
+	cp $(LOCAL_PATH)/wifi/WCNSS_cfg.dat \
+	$(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
+	cp $(LOCAL_PATH)/wifi/WCNSS_qcom_sdio_cfg.ini $(TARGET_OUT_ETC)/wifi/; \
+        ln -sf /system/etc/wifi/WCNSS_qcom_sdio_cfg.ini \
+	$(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
 endif
